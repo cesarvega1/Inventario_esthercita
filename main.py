d920e21fb587474c0b86e5d07071f1c17011f776
Cesar Vega Morales <cesarvegamrales@gmail.como>
2026-03-29 17:37:07 -0500

    se agregan validaciones y menu principal

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#Inventario
+from src.validacion_de_datos import *
+
+#MENU PRINCIPAL
+
+
+
+print("Proyecto de inventario de productos")
+print("1. Agregar producto")
+print("2. Mostrar inventario")
+print("3. Calcular estadistica")
+print("4. Salir")
+
+opcion = (input("Seleccione una opcion: "))
+
+
+if opcion == "1":  
+    agregar_producto()
+    print("Producto agregado correctamente")
+
+elif opcion == "2":
+    mostrar_inventario()
+
+elif opcion == "3":
+    calcular_estadistica()
+
+elif opcion == "4":
+    print("Saliendo del programa")
+
+else:
+    print("x Opcion invalida, intente nuevamente ")
+
